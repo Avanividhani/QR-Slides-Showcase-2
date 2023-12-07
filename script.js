646ca43fa570967b32b53e0b7212e70bc2228b28
Avanividhani <70509863+Avanividhani@users.noreply.github.com>
2023-12-07 21:23:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ $('[data-fancybox="gallery"]').fancybox({
 // Trigger click on the first image
 $('[data-fancybox="gallery"]:first').trigger('click');
 
-$('.download-button a').off('click');
 
 
 
